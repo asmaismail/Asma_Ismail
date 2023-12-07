8b022a4340c5a68dc205059771b6d3c732cd2500
asmaismail <asmaismail202020@gmail.com>
2023-12-07 21:02:08 +0500

    update

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <>
           <Scroll/>
-    <div className='bottom-0 fixed w-full'>
+    <div className='bottom-0 w-full'>
         <p className='border-b-2 border-white mx-4 w-auto'></p>
         <p className='flex content-center text-white items-center justify-center text-center mb-2'>Copyright © 2023.All rights are reserved.</p>
     </div>
