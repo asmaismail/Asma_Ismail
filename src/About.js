246adbd3ec2c1c274637a80f6f3f436b53b02399
asmaismail <asmaismail202020@gmail.com>
2023-12-07 21:40:33 +0500

    update

--- src/About.js
+++ src/About.js
@@ -1,5 +1,6 @@
 import React from "react";
 import img from "./Assets/about-me.webp";
+import CV from "./Assets/CV.pdf";
 
 const About = () => {
   return (
@@ -14,11 +15,10 @@ const About = () => {
           <img src={img} alt="img" className="h-72 lg:ml-20 mx-2  rounded-lg" />
         </div>
         <div className="lg:w-1/2 w-full">
-          <p className="text-white lg:mr-8 text-sm">
+          <p className="text-white lg:mr-24 text-sm flex text-justify lg:mt-0 mt-4">
             Hi! I'm Asma Ismail with having a 1.5 years of experince in
-            development of responsive <br />
-            and pixel perfect design from figma, having in depth knowledge in
-            React JS .
+            development of responsive and pixel perfect design from figma,
+            having in depth knowledge in React JS .
           </p>
           <span className="flex flex-row gap-x-2 mt-4">
             <p className="text-[#05C7AE]">Name:</p>
@@ -28,6 +28,11 @@ const About = () => {
             <p className="text-[#05C7AE]">Email:</p>
             <p className="text-white">asmaismail202020@gmail.com</p>
           </span>
+          <a href={CV} download={CV}>
+            <button className="text-white bg-[#05C7AE] py-2 px-3 rounded-md mt-6 hover:shadow-2xl shadow:[#05C7AE]">
+              Download Resume
+            </button>
+          </a>
         </div>
       </div>
     </>
